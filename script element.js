c1b1a7cfe4562790343f0ff17dfea6a35cfd6863
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-28 19:10:30 +0300

    problem has been npt correct

--- script element.js
+++ script element.js
@@ -1,3 +1,12 @@
-var menuToggle = document.getElementById("menu-toggle");
-var activeElement = document.getElementById(".active-element");
+var toggledMenu = document.getElementById("menu-toggle");
+var activeElements = document.getElementById(".active-element");
 
+var toggledMenu = toggledMenu.addEventListener("click", function () {
+    // forEach is not supported in IE11
+    // activeElements.forEach(function(e){
+    //     e.classList.toggle("active");
+    // });
+    for (var activated = 0; activated < activeElements.length; activated++) {
+        activeElements[activated].classList.toggle("active");
+    }
+})
